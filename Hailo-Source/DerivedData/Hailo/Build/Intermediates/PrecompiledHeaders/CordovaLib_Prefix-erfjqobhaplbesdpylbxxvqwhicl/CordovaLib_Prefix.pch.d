e5f0a3524e0ccad2751cc5e4ae5cdcf441bbbe82
Wells Johnston <wellsjohnston@gmail.com>
2013-08-13 18:17:30 -0400

    deleting more files to clean it up

---
+++ Hailo-Source/DerivedData/Hailo/Build/Intermediates/PrecompiledHeaders/CordovaLib_Prefix-erfjqobhaplbesdpylbxxvqwhicl/CordovaLib_Prefix.pch.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/wellsj/Desktop/Projects/Hailo/Hailo-Source/CordovaLib/CordovaLib_Prefix.pch
---
+++ Hailo-Source/DerivedData/Hailo/Build/Intermediates/PrecompiledHeaders/Hailo-Prefix-aygsdcoobnkwmichuauimggifyuo/Hailo-Prefix.pch.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/wellsj/Desktop/Projects/Hailo/Hailo-Source/Hailo/Hailo-Prefix.pch
